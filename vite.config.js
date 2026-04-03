d8cb8533fc4b9683113dd834951bd98e248add8d
cch-code13 <2829659106@qq.com>
2026-04-03 21:07:44 +0800

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/palace-visual-tang-song/',
   plugins: [
     vue(),
     vueDevTools(),
